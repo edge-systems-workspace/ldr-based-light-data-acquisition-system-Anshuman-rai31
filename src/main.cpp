055433a6cf9b9cf1c718642809fb0de39f087ea0
Anshuman-rai31 <kausik2931@gmail.com>
2026-02-21 16:05:48 +0530

    add Doxygen documentation for LDR light data acquisition in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/**
+ * @file main.cpp
+ * @brief LDR-based light data acquisition for Arduino (Doxygen documentation).
+ *
+ * Reads the analog value from an LDR connected to analog pin A0 and
+ * converts it to a 0-100% light intensity value. Both the raw ADC value
+ * and the calculated percentage are printed to the Serial monitor every
+ * 300 ms.
+ *
+ * Hardware:
+ *  - LDR connected as part of a voltage divider to analog pin A0 (ldrpin)
+ *
+ * @author Anshuman Rai
+ * @date 2026-02-21
+ * @version 1.0
+ */
+
 #include <Arduino.h>
 #define ldrpin A0
 void setup() {
