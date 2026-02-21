17602b5644acd0fe6be4c919c5bf7feaa1d27d4f
Anshuman-rai31 <kausik2931@gmail.com>
2026-02-21 16:00:21 +0530

    add XML migration state and update main.cpp for LDR sensor functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #define ldrpin A0
-/*void setup() {
+void setup() {
     Serial.begin(9600);
     pinMode (ldrpin, INPUT);
-}*/
+}
 
-void loop() {
+/*void loop() {
     int ldrValue = analogRead(ldrpin);
     int lightPercent = map(ldrValue,0,1023,0,100);
     Serial.print("ldrValue:");
@@ -13,4 +13,4 @@ void loop() {
     Serial.print("  lightPercent:");
     Serial.println(lightPercent);
     delay(300);
-}
+}*/
